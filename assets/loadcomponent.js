773658b9dbb67956fe47283e5daae8d4966fb833
joe-spec <joeaustin898@gmail.com>
2025-08-15 12:44:19 +0100

    eight commit

---
+++ assets/loadcomponent.js
@@ -0,0 +1,8 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch(err => console.error(`Error loading ${file}:`, err));
+}
